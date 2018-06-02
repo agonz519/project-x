30a04b0e7ac3c8a5b642b3840668eff9577b577f
Geoff's Macbook <geoffsmacbook@Geoffss-MBP.attlocal.net>
2018-06-01 22:01:23 -0400

    Integrates video feed npm package

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname)));
 
+require('ar-drone-png-stream')(client, { port: 8020 });
+
 // Routes
 // =============================================================
 
